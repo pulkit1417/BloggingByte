3849593590d16a0cdb3fa021acde99262f1e3f41
pulkit1417 <gupta.pulkit2408@gmail.com>
2024-07-30 00:22:39 +0530

    application complete bugs fixing left

--- routes/blog.js
+++ routes/blog.js
@@ -2,6 +2,7 @@ const {Router} = require("express")
 const multer = require("multer");
 const Blog = require('../models/blog')
 const path = require("path")
+const Comment = require("../models/comment")
 
 const router = Router();
 
@@ -35,6 +36,28 @@ router.post('/add-new', upload.single('coverImage'), async (req, res)=>{
         createdBy: user._id
     })
     res.redirect(`/blog/${blog._id}`)
+});
+
+router.get("/:id", async (req, res) => {
+  const blog = await Blog.findById(req.params.id).populate("createdBy");
+  const comments = await Comment.find({ blogId: req.params.id }).populate(
+    "createdBy"
+  );
+
+  return res.render("blog", {
+    user: req.user,
+    blog,
+    comments,
+  });
+});
+
+router.post("/comment/:blogId", async (req, res) => {
+  await Comment.create({
+    content: req.body.content,
+    blogId: req.params.blogId,
+    createdBy: req.user._id,
+  });
+  return res.redirect(`/blog/${req.params.blogId}`);
 });
 
 module.exports = router
